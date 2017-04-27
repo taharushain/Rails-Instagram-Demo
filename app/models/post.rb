2d9b203487f274392f09b8d038edafa2b835dba6
Taha Rushain <taharushain@gmail.com>
2017-04-27 11:33:07 +0500

    Post index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   validates :image, presence: true
 
-  has_attached_file :image, styles: { :medium => "640x" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
 end
